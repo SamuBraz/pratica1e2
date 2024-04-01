b81a2c758bfd891600a29a5a8d1d26d113608ad0
SAMUEL SILVA <samuel.brazz@gmail.com>
2024-03-31 23:26:32 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const root = document.getElementById('login-container');
+
+function verificarDados() {
+    const emailInput = document.getElementById("email");
+    const senhaInput = document.getElementById("senha");
+
+    const email = emailInput.value;
+    const senha = senhaInput.value;
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("email invalidado");
+        return false;
+    }
+    if (senha.length < 8) {
+        alert("senha invalidado, deve ter no minimo 7 digitos");
+        return false;
+    }
+    return true;
+}
