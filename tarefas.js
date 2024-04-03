10bb7665a8801442e5cfad39faed2620d641f8ca
SAMUEL SILVA <samuel.brazz@gmail.com>
2024-04-02 23:16:03 -0300

    Ajustes finais

--- tarefas.js
+++ tarefas.js
@@ -1,6 +1,5 @@
 function adicionarTarefa() {
     let novaTarefa = prompt("Digite a nova tarefa:");
-
     if (novaTarefa) {
         let novaTarefaElemento = document.createElement("li");
         novaTarefaElemento.textContent = novaTarefa;
@@ -9,3 +8,13 @@ function adicionarTarefa() {
         listaTarefas.appendChild(novaTarefaElemento);
     }
 }    
+
+function removerTarefa() {
+    let indece = prompt("Digite o numero da lista que deseja apagar: ");
+    indice = parseInt(indece);
+    indice -=1;
+    if(indece){
+        let listaTarefas = document.getElementById("lista-tarefas");
+        let elementoRemovido = listaTarefas.removeChild(listaTarefas.children[indice]);
+  }
+}  
